c2bf3d2b16eb06bd852c41686d348b6fe852db80
yepes1201 <yepes1201@gmail.com>
2022-07-08 17:10:34 -0500

    testing socket endpoint

--- src/components/Socket/Socket.js
+++ src/components/Socket/Socket.js
@@ -1,5 +1,6 @@
 import io from "socket.io-client";
 
-const socket = io(process.env.REACT_APP_SOCKET_URL);
+// const socket = io(process.env.REACT_APP_SOCKET_URL);
+const socket = io("https://wechat-app-danielyepes.herokuapp.com");
 
 export default socket;
